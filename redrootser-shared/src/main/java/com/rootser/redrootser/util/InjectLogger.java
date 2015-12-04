ce31477651677e56f23cdf2c82b2660be60a11f5
John Hancock <jhancock1975@gmail.com>
2015-12-03 22:40:00 -0500

    adding redrootser-shared

---
+++ redrootser-shared/src/main/java/com/rootser/redrootser/util/InjectLogger.java
@@ -0,0 +1,7 @@
+package com.rootser.redrootser.util;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectLogger {
+}
---
+++ redrootser-shared/src/main/java/com/rootser/redrootser/util/PrintYears.java
@@ -0,0 +1,12 @@
+package com.rootser.redrootser.util;
+
+public class PrintYears {
+	public static void main(String[] args) {
+		int curYear = 1900;
+		for( int i=0; curYear <= 2015; i++){
+			System.out.println(curYear);
+			curYear=curYear + 1;
+		}
+	}
+
+}
