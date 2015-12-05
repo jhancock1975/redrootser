b74adfa01a23ff976057bcc576dfcb25f237a0d1
John Hancock <jhancock1975@gmail.com>
2015-12-05 08:18:44 -0500

    fix spring boot web multiple dependency in rootser-db, properly annotate CustomerEventHandler.handleCustomerSave so it is called when doing http post to save entity

--- redrootser-db/src/main/java/com/rootser/redrootser/handlers/CustomerEventHandler.java
+++ redrootser-db/src/main/java/com/rootser/redrootser/handlers/CustomerEventHandler.java
@@ -2,7 +2,7 @@ package com.rootser.redrootser.handlers;
 
 
 import org.slf4j.Logger;
-import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +10,12 @@ import com.rootser.redrootser.entities.Customer;
 import com.rootser.redrootser.util.InjectLogger;
 
 @Component
-@RepositoryEventHandler
+@RepositoryEventHandler(Customer.class)
 public class CustomerEventHandler {
 	private static @InjectLogger Logger logger;
-	@HandleBeforeSave
+	@HandleBeforeCreate
 	public void handleCustomerSave(Customer customer){
 		logger.info("handling customer save");
-		System.out.println("handling customer save");
 	}
 
 }
